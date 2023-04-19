bb5d251d6e1a217132b8b42383235d416d3faf00
KPSawhney <kylesawhney@outlook.com>
2023-04-18 23:12:50 -0700

    Refactor

--- background.js
+++ background.js
@@ -1,19 +1,28 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'getLoginStatus') {
-    console.log('Checking Poshmark login status...');
-    checkPoshmarkLoginStatus().then((poshmarkLoggedIn) => {
-      console.log('Poshmark login status:', poshmarkLoggedIn ? 'Logged In' : 'Not Logged In');
-
-      console.log('Checking Google login status...');
-      checkGoogleLoginStatus().then((googleLoggedIn) => {
-        console.log('Google login status:', googleLoggedIn ? 'Logged In' : 'Not Logged In');
-
-        const isLoggedIn = poshmarkLoggedIn || googleLoggedIn;
-        console.log('Sending login status:', isLoggedIn ? 'Logged In' : 'Not Logged In');
-        sendResponse({ isLoggedIn });
-      });
+async function checkPoshmarkLoginStatus() {
+  return new Promise((resolve) => {
+    chrome.cookies.get({ url: 'https://poshmark.com', name: 'MUID' }, (cookie) => {
+      resolve(!!cookie);
     });
+  });
+}
 
-    return true; // Required for async response handling
+async function checkGoogleLoginStatus() {
+  return new Promise((resolve) => {
+    chrome.cookies.get({ url: 'https://poshmark.com', name: 'G_AUTHUSER_H' }, (cookie) => {
+      resolve(!!cookie);
+    });
+  });
+}
+
+chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+  if (request.type === 'getPoshmarkLoginStatus') {
+    console.log('Checking Poshmark login status...');
+    const poshmarkLoggedIn = await checkPoshmarkLoginStatus();
+    console.log('Checking Google login status...');
+    const googleLoggedIn = await checkGoogleLoginStatus();
+    const isLoggedIn = poshmarkLoggedIn || googleLoggedIn;
+    console.log(`Sending login status: ${isLoggedIn ? 'Logged In' : 'Not Logged In'}`);
+    sendResponse({ isLoggedIn });
   }
+  return true;
 });
--- popup.js
+++ popup.js
@@ -2,17 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateLoginStatus() {
     chrome.runtime.sendMessage({ type: 'getPoshmarkLoginStatus' }, (response) => {
       const isLoggedIn = response.isLoggedIn;
-      console.log('Received login status:', isLoggedIn ? 'Logged In' : 'Not Logged In'); // Add this log
+      console.log(`Received login status: ${isLoggedIn ? 'Logged In' : 'Not Logged In'}`);
       document.getElementById('loginStatus').textContent = isLoggedIn ? 'Logged In' : 'Not Logged In';
-      document.getElementById('openPoshmark').dataset.isLoggedIn = isLoggedIn;
     });
   }
 
   updateLoginStatus();
 
   document.getElementById('openPoshmark').addEventListener('click', () => {
-    const isLoggedIn = document.getElementById('openPoshmark').dataset.isLoggedIn === 'true';
-    const url = isLoggedIn ? 'https://poshmark.com/my_account' : 'https://poshmark.com/login';
-    chrome.tabs.create({ url });
+    chrome.tabs.create({ url: 'https://poshmark.com' });
   });
 });
