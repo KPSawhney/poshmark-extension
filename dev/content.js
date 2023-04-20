e5355c16fb0ea6eeade1044bb6af24f9cec461d8
KPSawhney <kylesawhney@outlook.com>
2023-04-20 11:48:56 -0700

    Additional commenting

--- dev/content.js
+++ dev/content.js
@@ -1,5 +1,7 @@
+// Log when content.js is loaded
 console.info('content.js loaded');
 
+// Parse the closet URL from the current Poshmark page
 function parseClosetURL() {
   const closetLinks = Array.from(document.querySelectorAll('a.dropdown__link'));
   const closetLink = closetLinks.find(link => link.textContent.trim() === 'My Closet');
@@ -14,7 +16,9 @@ function parseClosetURL() {
 const closetURL = parseClosetURL();
 if (closetURL) {
   console.info('Parsed closet URL:', closetURL);
+  // Send the closet URL to background.js
   chrome.runtime.sendMessage({ type: 'closetURL', url: closetURL });
+  // Save the closet URL to local storage
   chrome.storage.local.set({ closetURL: closetURL });
 } else {
   console.error('Failed to parse closet URL');
