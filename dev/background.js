0717424e6054a0a11f6fcde5a3dd8712d30a2091
KPSawhney <kylesawhney@outlook.com>
2023-04-21 12:51:18 -0700

    Dev updates and adds prod backup

--- dev/background.js
+++ dev/background.js
@@ -3,16 +3,28 @@ console.info('background.js loaded');
 
 // Check Poshmark login status using jwt and ui cookies
 function checkPoshmarkLoginStatus(callback) {
+  // Log that we are checking Poshmark login status
   console.info('Checking Poshmark login status...');
-  chrome.cookies.get({ url: 'https://poshmark.com', name: 'jwt' }, function (jwtCookie) {
-    chrome.cookies.get({ url: 'https://poshmark.com', name: 'ui' }, function (uiCookie) {
-      let poshmarkLoginStatus;
+
+  // Get the jwt and ui cookies from Poshmark
+  chrome.cookies.get({
+    url: 'https://poshmark.com',
+    name: 'jwt'
+  }, function (jwtCookie) {
+    chrome.cookies.get({
+      url: 'https://poshmark.com',
+      name: 'ui'
+    }, function (uiCookie) {
+      // If both cookies are present, the user is logged in
       if (jwtCookie && uiCookie) {
         poshmarkLoginStatus = 'Logged In';
       } else {
         poshmarkLoginStatus = 'Not Logged In';
       }
+      // Log the Poshmark login status
       console.info('Poshmark login status:', poshmarkLoginStatus);
+
+      // Call the callback function with the Poshmark login status
       callback(poshmarkLoginStatus);
     });
   });
@@ -20,38 +32,62 @@ function checkPoshmarkLoginStatus(callback) {
 
 // Get closet URL from local storage
 async function getClosetURL() {
+  // Log that we are getting the closet URL from local storage
+  console.info('Getting closet URL from local storage...');
+
+  // Try to get the closet URL from local storage
   return new Promise((resolve) => {
-    chrome.storage.local.get(['closetURL'], function (result) {
-      resolve(result.closetURL);
+    chrome.storage.local.get('closetURL', function (data) {
+      if (chrome.runtime.lastError) {
+        // Log the error
+        console.error('Error getting closet URL from local storage:', chrome.runtime.lastError);
+        resolve(null);
+      } else {
+        // Log the closet URL
+        console.info('Closet URL from local storage:', data.closetURL);
+
+        // Return the closet URL
+        resolve(data.closetURL);
+      }
     });
   });
 }
 
-// Listen for connection from popup.js
+// Listen for connection from popup.js to send login status and closet URL
 chrome.runtime.onConnect.addListener(function (port) {
+  // If the connection is from the popup.js script with the name "getPoshmarkData",
+  // check the Poshmark login status and get the closet URL
   if (port.name === "getPoshmarkData") {
     checkPoshmarkLoginStatus(async function (status) {
       const myClosetURL = await getClosetURL();
-      port.postMessage({ isLoggedIn: status === 'Logged In', closetURL: myClosetURL });
+      port.postMessage({
+        isLoggedIn: status === 'Logged In',
+        closetURL: myClosetURL
+      });
     });
   }
 });
 
+// Store closet URL received from content.js
 let myClosetURL = null;
 
-// Listen for messages from content.js
+// Listen for messages from content.js to update myClosetURL
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'getLoginStatus') {
-    checkPoshmarkLoginStatus(function (status) {
-      sendResponse({ status: status });
-    });
-    return true;
-  }
-  if (message.type === 'getClosetURL') {
-    sendResponse({ url: myClosetURL });
-  }
+  // If the message type is "closetURL",
+  // update myClosetURL with the closet URL
   if (message.type === 'closetURL') {
     myClosetURL = message.url;
     console.info('Received closet URL:', myClosetURL);
   }
+  else if (message.type === 'flashMessage') {
+    console.log('Flash message detected:', message.content);
+    // Send the flash message to popup.js
+    chrome.runtime.sendMessage({ type: 'flashMessage', content: message.content });
+  }
 });
+
+else if (message.type === 'flashMessage') {
+  console.log('Flash message detected:', message.content);
+  // Send the flash message to popup.js
+  chrome.runtime.sendMessage({ type: 'flashMessage', content: message.content });
+}
