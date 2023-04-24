25714d82dfac4c4b1593d353cd603e0bdebfe0ca
KPSawhney <kylesawhney@outlook.com>
2023-04-23 17:34:40 -0700

    Create popup functions

--- prod/functions/popup_functions.js
+++ prod/functions/popup_functions.js
@@ -3,6 +3,31 @@
 // This function executes the shareToFollowers function from `content.js`, and returns
 // a success animation on success
 
+function openMyCloset(myClosetURL) {
+  if (myClosetURL) {
+    console.info("Opening My Closet...");
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tab = tabs[0];
+
+      // Check if the current tab is a Poshmark page
+      if (tab.url.includes("poshmark.com")) {
+        // Update the current tab's URL to the closet URL
+        chrome.tabs.update({ url: myClosetURL }, (updatedTab) => {
+          console.info("My Closet opened on current tab:", updatedTab.id);
+        });
+      } else {
+        // Open the closet URL in a new tab
+        chrome.tabs.create({ url: myClosetURL }, (newTab) => {
+          console.info("My Closet opened in new tab:", newTab.id);
+        });
+      }
+    });
+  } else {
+    console.warn("Closet URL not available");
+  }
+};
+
 function shareItemsToFollowers(callback) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const tab = tabs[0];
@@ -26,4 +51,4 @@ function shareItemsToFollowers(callback) {
 }
 
 // Explicitly export the functions
-export { shareItemsToFollowers };
+export { shareItemsToFollowers, openMyCloset };
