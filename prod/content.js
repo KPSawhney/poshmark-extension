757c251c3879054a23688059f40695e96d7c8385
KPSawhney <kylesawhney@outlook.com>
2023-04-24 18:57:29 -0700

    Prod

--- prod/content.js
+++ prod/content.js
@@ -27,49 +27,45 @@ function parseClosetURL() {
 // Share all items to followers by clicking on various elements in order.
 
 async function shareItemsToFollowers(sendResponse) {
-  const steps = [
-    {
-      selector: "i.icon.icon-bulk-tools",
-      action: "click",
-      description: "Spanner icon",
-      errorMessage: "Error: Spanner icon not found",
-    },
-    {
-      selector: 'div[data-et-name="share_to_followers"]',
-      action: "click",
-      description: "Share to followers button",
-      errorMessage: "Error: Share to Followers dropdown choice not found",
-      delay: 1000,
-    },
-    {
-      selector: ".tile__checkbox",
-      action: "click",
-      description: "Select all checkbox",
-      errorMessage: "Error: Select All checkbox not found",
-      delay: 1000,
-    },
-    {
-      selector: 'button[data-et-name="share_to_followers"]',
-      action: "click",
-      description: "Green share button",
-      errorMessage: "Error: Green share button not found",
-      delay: 1000,
-    },
-  ];
-
   try {
-    for (const step of steps) {
-      if (step.delay) {
-        await new Promise((resolve) => setTimeout(resolve, step.delay));
-      }
-      const element = document.querySelector(step.selector);
-      console.log(step.description + ":", element);
-      if (!element) {
-        throw new Error(step.errorMessage);
-      }
-      element[step.action]();
+    // Step 1: Click on the spanner icon
+    const spannerIcon = document.querySelector("i.icon.icon-bulk-tools");
+    console.log("Spanner icon:", spannerIcon);
+    if (!spannerIcon) {
+      throw new Error("Error: Spanner icon not found");
     }
+    spannerIcon.click();
 
+    // Step 2: Click share to followers dropdown choice
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // wait for 1 second
+    const shareToFollowersButton = document.querySelector(
+      'div[data-et-name="share_to_followers"]'
+    );
+    console.log("Share to followers button:", shareToFollowersButton);
+    if (!shareToFollowersButton) {
+      throw new Error("Error: Share to Followers dropdown choice not found");
+    }
+    shareToFollowersButton.click();
+
+    // Step 3: Click the checkbox to select all clothes after the page reloads
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // wait for 1 second
+    const selectAllCheckbox = document.querySelector(".tile__checkbox");
+    console.log("Select all checkbox:", selectAllCheckbox);
+    if (!selectAllCheckbox) {
+      throw new Error("Error: Select All checkbox not found");
+    }
+    selectAllCheckbox.click();
+
+    // Step 4: Click the green share button
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // wait for 1 second
+    const greenShareButton = document.querySelector(
+      'button[data-et-name="share_to_followers"]'
+    );
+    console.log("Green share button:", greenShareButton);
+    if (!greenShareButton) {
+      throw new Error("Error: Green share button not found");
+    }
+    greenShareButton.click();
     console.log("Green share button clicked successfully.");
     sendResponse({ success: true, message: "Green share button clicked." });
   } catch (error) {
@@ -78,6 +74,7 @@ async function shareItemsToFollowers(sendResponse) {
     sendResponse({ success: false, error: error.message });
   }
 }
+
 // Parse the closet URL and sending it to the background script
 
 const closetURL = parseClosetURL();
